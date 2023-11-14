6611f1ab7c546981b8ac932c5ece9df1dcbc6a14
koxnjeez <145009796+koxnjeez@users.noreply.github.com>
2023-11-14 02:09:14 +0200

    Add files via upload

---
+++ hw 09.11/hw 09.11/hw 09.11.cpp
@@ -0,0 +1,19 @@
+﻿#include "settings.h"
+
+int main()
+{
+	char* proposal = new char[50];
+	int length = 50;
+	int spaces = 0;
+	cout << "Input the proposal: ";
+	gets_s(proposal, length);
+	float symbols = strlen(proposal);
+	for (int i = 0; i < symbols; i++) {
+		if (proposal[i] == (char)32) {
+			spaces++;
+		}
+	}
+	symbols -= spaces;
+	cout << symbols / (spaces + 1);
+	Sleep(INFINITE);
+}
